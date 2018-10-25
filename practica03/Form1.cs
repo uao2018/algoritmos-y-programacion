5b7d1f706a10fe61c094a868064556130da55c43
Luis Garreta <lgarreta@uao.edu.co>
2018-10-24 20:40:46 -0500

    pr03

---
+++ practica03/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+public partial class Form1 : Form
+{
+	public Form1() 	{
+		InitializeComponent();
+	}
+
+	private void btSalir_Click(object sender, EventArgs e) 	{
+		Close();
+	}
+
+	private void btIniciar_Click(object sender, EventArgs e) {
+		principal();
+	}
+
+	public static void principal () {
+		int N;
+		string reporte;
+
+		int[] arrPuntajes;
+		string[] arrNombres;
+
+		N = Convert.ToInt32(Interaction.InputBox("Cuantos Jugadores: "));
+		arrNombres = new string[N];
+		arrPuntajes = new int[N];
+
+		Funciones.leerDatos (arrNombres, arrPuntajes, N);
+		reporte = Funciones.crearReporte(arrNombres, arrPuntajes, N);
+		MessageBox.Show ("Reporte sin ordenar: \n" + reporte);
+
+		Funciones.ordenamientoBurbujaPorPuntajes(arrNombres, arrPuntajes, N); // solo llamarlo. Supone que ya existe
+		reporte = Funciones.crearReporte(arrNombres, arrPuntajes, N);
+		MessageBox.Show ("Reporte ordenado por puntaje: \n" + reporte);
+	}
+}
---
+++ practica03/Funciones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+class Funciones {
+	//public static void leerDatos (string[] arrNom, int[] arrPun, int N) {
+	
+
+	//public static string crearReporte(string[] arrNom, int[] arrPunt, int N) {
+	
+	public static void ordenamientoBurbujaPorPuntajes(string[] arrNom, int[] arrPun, int N)	{
+		// Variables temporales: 
+		String tempAlfabetico;
+		int tempNumerico;
+		for (int i = 0; i < N - 1; i++)		{
+			for (int j = 0; j < N - 1 - i; j++)			{
+				if (arrPun[j] > arrPun[j + 1])				{
+					// Intercambio: Ordena los estudiantes según la calificación --> 
+					
+					tempNumerico = arrPun[j];
+					arrPun[j] = arrPun[j + 1];
+					arrPun[j + 1] = tempNumerico;
+					// Intercambio: Ordena los arrNom según la calificación --> 
+					tempAlfabetico = arrNom[j];
+					arrNom[j] = arrNom[j + 1];
+					arrNom[j + 1] = tempAlfabetico;					
+				}
+			}   // if 
+		}   // for j  
+	}   // for i                     
+} 
+
